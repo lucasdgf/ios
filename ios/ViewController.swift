6644e0570408a576355b1736f086da8381589af2
lucasdgf <lucasdgf@gmail.com>
2017-04-15 19:45:56 -0700

    Added iOS project.

---
+++ ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios
+//
+//  Created by Lucas Freitas on 4/15/17.
+//  Copyright © 2017 Everyday Activism. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
